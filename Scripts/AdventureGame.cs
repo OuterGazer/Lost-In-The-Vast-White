a1da53b1e24531610aa6dd1cbdc940884d428d69
OuterGazer <71871620+OuterGazer@users.noreply.github.com>
2021-08-17 08:17:02 +0200

    Gameplay source code

---
+++ Scripts/AdventureGame.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.state = this.startingState;
+        this.textComponent.text = this.state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q) || Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        this.ManageState();
+    }
+
+    private void ManageState()
+    {
+        State[] nextStates = this.state.GetNextStates();
+
+        for(int i = 0; i < nextStates.Length; i++)
+        {
+            if (Input.GetKeyDown(KeyCode.Keypad1 + i) || Input.GetKeyDown(KeyCode.Alpha1 + i))
+            {
+                this.state = nextStates[i];
+            }
+        }        
+
+        this.textComponent.text = this.state.GetStateStory();
+    }
+}
